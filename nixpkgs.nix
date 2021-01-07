ec49591b4255d34e90f6cf6e3dbfa8c9f57b07d4
Chris Warburton <chris@zipabout.com>
2021-01-07 04:22:03 +0000

    Bump nixpkgs for Big Sur dylib fix

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -3,8 +3,8 @@ with {
   src = (import <nixpkgs> {}).fetchFromGitHub {
     owner  = "NixOS";
     repo   = "nixpkgs";
-    rev    = "d529175";
-    sha256 = "0mhqhq21y5vrr1f30qd2bvydv4bbbslvyzclhw0kdxmkgg3z4c92";
+    rev    = "9dd4dda";
+    sha256 = "0a5n1122mwwm2ndlr6y5b8x6mi6mja1dw5widaw9sn323aznr800";
   };
 };
 import "${src}"
